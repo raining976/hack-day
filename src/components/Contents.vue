6ccff7bc2ec2813787c743a4d6a1bbbbadf03aba
raining <running.zhang@foxmail.com>
2024-03-29 00:37:21 +0800

    feat：添加信息说明的框架

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,4 +1,91 @@
 <template>
-内容样式模板
-<!-- <Raining></Raining> -->
+    <div class="contentContainer">
+        <div class="leftLogo">
+            <img src="@/assets/icons/hackday.png" alt="">
+        </div>
+        <div class="infoContainer">
+            <div class="title">
+                {{ infoItem.index }}
+                <span class="line"></span>
+                {{ infoItem.title }}
+            </div>
+            <div class="infoContent"></div>
+        </div>
+    </div>
+    <!-- <Raining></Raining> -->
 </template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+    infoItem: {
+        title: String,
+        index: String,
+    }
+})
+
+
+
+</script>
+
+
+
+<style lang="scss" scoped>
+.contentContainer {
+    @include flex-between();
+    position: absolute;
+    bottom: 2px;
+    right: 2px;
+    width: 86vw;
+    height: 85vh;
+    background: #FFFFFF;
+    border-radius: 15px 0px 0px 0px;
+    border: 2px solid #000000;
+
+    .leftLogo {
+        height: 100%;
+        @include flex-center();
+
+        img {
+            width: 117.1px;
+            height: 77vh;
+        }
+    }
+
+    .infoContainer {
+        $topHeight: 10.4vw;
+        width: 76.4vw;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+
+        .title {
+            width: 100%;
+            height: $topHeight;
+            color: #000000;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            font-size: 3.3vw;
+            font-family: lattice;
+
+            .line {
+                width: 0.8vw;
+                height: 3.3vw;
+                background: #151515;
+                margin: 0 10px;
+                margin-right: 20px;
+            }
+        }
+
+        .infoContent {
+            width: 76.4vw;
+            height: calc(100% - $topHeight);
+            background: #F9F9F9;
+            border-radius: 15px 15px 15px 15px;
+            border: 2px solid #000000;
+        }
+
+    }
+}
+</style>
--- src/views/ContentsView.vue
+++ src/views/ContentsView.vue
@@ -1,4 +1,81 @@
 <template>
-内容
-<contents></contents>
+    <div class="contentBlock">
+        <div class="leftBar">
+            <div class="list">
+                <div @click="tabClicked(index)" class="item" :class="{ active: activeIndex == index }"
+                    v-for="(text, index) in items" :key="index">
+                    {{ text }}
+                </div>
+            </div>
+        </div>
+        <contents :infoItem="{ title: items[activeIndex], index: '0' + (activeIndex + 1) }"></contents>
+    </div>
+
 </template>
+<script setup>
+import { ref } from 'vue'
+
+const items = ref(['介绍', '流程', '奖项', '问题', '我们'])
+const activeIndex = ref(0);
+
+const tabClicked = (i) => {
+    activeIndex.value = i;
+}
+
+
+</script>
+<style lang="scss" scoped>
+.contentBlock {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    $paddingLeft: 3.6vw;
+    $paddingTop: 14vh;
+    background-color: $theme-light-color;
+    width: calc(100vw - $paddingLeft);
+    height: calc(100vh - $paddingTop);
+    padding-top: $paddingTop;
+    padding-left: $paddingLeft;
+
+    .leftBar {
+        width: 12vh;
+        height: 80vh;
+        background: #151515;
+        box-shadow: -1vw 0.3vw 1.6vw 0.1vw rgba(0, 0, 0, 0.17);
+        border-radius: 0.8vw;
+
+        .list {
+            $padding: 5.2vw;
+            padding: $padding 0;
+            width: 100%;
+            height: calc(100% - 2 * $padding);
+            @include flex-between(true);
+
+            .item {
+                position: relative;
+                font-family: lattice;
+                font-weight: 400;
+                font-size: 28.8px;
+                color: #FFFFFF47;
+                cursor: pointer;
+
+                &.active {
+                    color: #fff;
+
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        bottom: -5px;
+                        left: 50%;
+                        transform: translateX(-50%);
+                        width: 17px;
+                        height: 4px;
+                        background-color: #fff;
+                    }
+                }
+            }
+        }
+    }
+
+}
+</style>
